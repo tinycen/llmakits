4c0b210370ed09254057b3c242e35a7480c2d885
tinycen <sky_ruocen@qq.com>
2025-09-18 10:42:52 +0800

    build: 更新项目名称为'llmkits'
    修复项目名称拼写错误，从'llmkit'更正为'llmkits'以保持一致性

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except FileNotFoundError:
     install_requires = []
 
 setup(
-    name='llmkit',
+    name='llmkits',
     version='0.1.0',
     packages=find_packages(),
     install_requires=install_requires,
