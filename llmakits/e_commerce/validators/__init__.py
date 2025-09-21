d3ac56f4063a87d1d964f348c06f9ac7727a2e00
tinycen <sky_ruocen@qq.com>
2025-09-21 14:23:46 +0800

    refactor(validators): 优化验证器模块的导入和导出
    移除不再使用的check_tag_closing和validate_value_exists函数
    添加新的validate_dict和validate_string验证函数

--- llmakits/e_commerce/validators/__init__.py
+++ llmakits/e_commerce/validators/__init__.py
@@ -3,14 +3,14 @@
 包含HTML验证、字符串验证、值验证等功能
 """
 
-from .html_validator import check_allowed_tags, check_tag_closing
+from .html_validator import check_allowed_tags
 from .string_validator import contains_chinese, remove_chinese
-from .value_validator import validate_value_exists
+from .value_validator import validate_dict, validate_string
 
 __all__ = [
     'check_allowed_tags',
-    'check_tag_closing', 
     'contains_chinese',
     'remove_chinese',
-    'validate_value_exists'
+    'validate_dict',
+    'validate_string',
 ]
