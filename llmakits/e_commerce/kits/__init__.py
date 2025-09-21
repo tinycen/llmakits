b98b3f7862e3a80111754c4ff1dcbb298431787c
tinycen <sky_ruocen@qq.com>
2025-09-21 18:21:54 +0800

    feat(attribute_kit): 添加 fill_attr 到 __all__ 列表
    将 fill_attr 函数添加到 __all__ 列表以公开该功能

--- llmakits/e_commerce/kits/__init__.py
+++ llmakits/e_commerce/kits/__init__.py
@@ -1,4 +1,5 @@
 from .title_kit import check_title, generate_title
 from .des_kit import generate_html
+from .attribute_kit import fill_attr
 
-__all__ = ['check_title', 'generate_title', 'generate_html']
+__all__ = ['check_title', 'generate_title', 'generate_html', 'fill_attr']
