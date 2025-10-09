1e0f991d03a2620d80167939f202af4f0c1e4328
tinycen <sky_ruocen@qq.com>
2025-10-09 23:44:08 +0800

    chore: 更新版本号至v0.4.4
    同步修改create_tag.sh和setup.py中的版本号，保持一致性

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except FileNotFoundError:
 
 setup(
     name='llmakits',
-    version='0.4.3',
+    version='0.4.4',
     packages=find_packages(),
     install_requires=install_requires,
     author='tinycen',
