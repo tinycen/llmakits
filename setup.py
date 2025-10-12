b7aea253e44711831dced5407b00f38b0f916cf3
tinycen <sky_ruocen@qq.com>
2025-10-12 12:29:42 +0800

    fix: 回退版本号至v0.4.6
    同步修改create_tag.sh和setup.py中的版本号，保持一致性

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except FileNotFoundError:
 
 setup(
     name='llmakits',
-    version='0.4.7',
+    version='0.4.6',
     packages=find_packages(),
     install_requires=install_requires,
     author='tinycen',
