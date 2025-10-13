a7f74f1531feda67a830c6b7b10cc91a50522844
tinycen <sky_ruocen@qq.com>
2025-10-13 18:53:55 +0800

    chore(release): 更新版本号至 v0.5.1
    - 修改 create_tag.sh 中的 TAG_NAME为 v0.5.1
    - 同步更新 setup.py 中的 version 为 0.5.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except FileNotFoundError:
 
 setup(
     name='llmakits',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     install_requires=install_requires,
     author='tinycen',
