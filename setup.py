2865ca6c094fe9304dfa3510ab039c2ce4eb1a4e
tinycen <sky_ruocen@qq.com>
2025-10-17 13:54:39 +0800

    fix: 修正dashscope平台下模型名称大小写不一致问题

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except FileNotFoundError:
 
 setup(
     name='llmakits',
-    version='0.5.7',
+    version='0.5.8',
     packages=find_packages(),
     install_requires=install_requires,
     author='tinycen',
